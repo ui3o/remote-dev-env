6d73e8cc8ceeb107d513093b5c0b7c52546dd568
ui3o.com <ui3o.com@gmail.com>
2023-05-06 03:41:24 +0200

    build fix

--- .config/etc/install.sh
+++ .config/etc/install.sh
@@ -68,6 +68,7 @@ sed -i "s|</head>|\
 
 # install oh-my-zsh
 echo install oh-my-zsh...
+mkdir -p /home/podman/npm
 chown podman:podman -R /home/podman
 mv /home/podman/.oh-my-zsh /tmp
 su - podman -c 'sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended'
