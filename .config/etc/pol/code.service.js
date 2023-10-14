2b696f7fbbb95399f0aac7e2e0d7461c32528083
ui3o.com <ui3o.com@gmail.com>
2023-10-14 11:30:39 +0200

    code.service environment pass fix

--- .config/etc/pol/code.service.js
+++ .config/etc/pol/code.service.js
@@ -5,8 +5,8 @@ service.setup = {
     /** @param ss service tools */
     onStart: async (ss) => {
         const privatePass = await ss.cli.splitByLine.do('cat', '/run/secrets/pp.store');
-        process.env.PASSWORD = !privatePass.c ? privatePass.o[0] : 'podman';
-        process.env.SHELL = '/bin/zsh';
+        ss.env.PASSWORD = !privatePass.c ? privatePass.o[0] : 'podman';
+        ss.env.SHELL = '/bin/zsh';
         ss.exec.uid('podman').gid('podman').wd("/home/podman")
             .do(`sudo`, '-E', `-u`, `podman`, '/usr/bin/code-server');
     },
