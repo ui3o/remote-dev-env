b074b3cbb2caeaafd9a8fa45e16df15678c6aaaf
ui3o.com <ui3o.com@gmail.com>
2025-08-15 17:07:29 +0200

    add: create config.py with initial root path and configuration structure

---
+++ config/etc/linky/config.py
@@ -0,0 +1,8 @@
+root_path = "/mine/.layers"
+# Key: Destination, Value: Source
+config = {
+    # If you would like to bring your own config for bash you can do so like this:
+    # The key defines the destination you want to have your final conf file as a symlink
+    # The Value has to point to you own config file's path in your persistance storege
+    # "/root/.bashrc": "/mine/.layers/root/.bashrc",
+}
