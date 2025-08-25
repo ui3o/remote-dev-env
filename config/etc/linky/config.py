ff2a70a75a492e27f2c98875a0f08d5312074b3d
ui3o.com <ui3o.com@gmail.com>
2025-08-25 09:12:35 +0200

    fix: linky workflow update and default config changed

--- config/etc/linky/config.py
+++ config/etc/linky/config.py
@@ -1,8 +1,6 @@
-root_path = "/mine/.layers"
-# Key: Destination, Value: Source
-config = {
-    # If you would like to bring your own config for bash you can do so like this:
-    # The key defines the destination you want to have your final conf file as a symlink
-    # The Value has to point to you own config file's path in your persistance storege
-    # "/root/.bashrc": "/mine/.layers/root/.bashrc",
-}
+# Mine default
+# First entry is the rootpath to the layer.
+layers = [
+    # Root path of the layer
+    "/mine/.layers",
+]
