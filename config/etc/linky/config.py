c6856785b97fd324918bc5924d6b1ee68317f6b6
ui3o <43638725+ui3o@users.noreply.github.com>
2026-03-30 07:00:01 +0000

    fix: correct layer path and comment out unused cleanup section in config

--- config/etc/linky/config.py
+++ config/etc/linky/config.py
@@ -1,18 +1,20 @@
 # Mine default
 # First entry is the rootpath to the layer.
+# Mine default
+# First entry is the rootpath to the layer.
 layers = [
     # Root path of the layer
-    "/layer",
+    "/mine/.layers",
     # soft sync layers to not overwrite all files under root dir
-    "-s /root",
+    # "-s /root",
     # hard sync overwrite every file under /etc/ssl/certs dir removing files that would not conflict otherwise
-    "-h /etc/ssl/certs",
+    # "-h /etc/ssl/certs",
 ]
 
-__cleanup__ = [
-    # First entry of the array where to move the files for cleanup
-    "/tmp/cleanup",
-    # File paths to cleanup after creating symlinks
-    "/root/path/to/remove",
-    "/other/path/to/remove",
-]
+# __cleanup__ = [
+#     # First entry of the array where to move the files for cleanup
+#     "/tmp/cleanup",
+#     # File paths to cleanup after creating symlinks
+#     "/root/path/to/remove",
+#     "/other/path/to/remove",
+# ]
